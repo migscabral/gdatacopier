b16b157b27c2770f09505921d7e4db192e3cb9c1
devraj <devraj@94ee8f2f-193c-0410-bb36-b14c205f76dc>
2008-11-06 22:54:14 +0000

    Added a package

---
+++ python/src/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+	gdatacopier.py, Copyright (c) 2007 De Bortoli Wines Pty Ltd
+	http://gdatacopier.googlecode.com/
+	Distributed under the terms and conditions of the GNU/GPL v3
+	
+	Version 1.1.0
+
+	Developed by Eternity Technologies Pty Ltd.
+	
+	__init__.py
+
+"""
