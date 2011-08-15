517f2dcf205f30fc0b3ea1b1434c850eb9cb2bec
devraj <devraj@94ee8f2f-193c-0410-bb36-b14c205f76dc>
2011-08-15 23:50:07 +0000

    Added basic skeleton for v3

---
+++ branches/3.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name="GDataCopier",
+	version="3.0",
+	description="",
+	author="Devraj Mukherjee, Matteo Canato",
+	author_email="devraj@gmail.com",
+	url="http://gdatacopier.googlecode.com",
+	packages=['gdatacopier']
+)
