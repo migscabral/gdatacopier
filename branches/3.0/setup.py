f74caa00217037109f2739903ff35337cb668fbf
devraj <devraj@94ee8f2f-193c-0410-bb36-b14c205f76dc>
2012-02-25 04:34:38 +0000

    Fixed setup file

--- branches/3.0/setup.py
+++ branches/3.0/setup.py
@@ -23,11 +23,11 @@
 from distutils.core import setup
 
 setup(
-	name="GDataCopier",
-	version="3.0",
-	description="",
-	author="Devraj Mukherjee, Matteo Canato",
-	author_email="devraj@gmail.com",
-	url="http://gdatacopier.googlecode.com",
-	packages=['gdatacopier']
+    name="GDataCopier",
+    version="3.0",
+    description="",
+    author="Devraj Mukherjee, Matteo Canato",
+    author_email="devraj@gmail.com",
+    url="http://gdatacopier.googlecode.com",
+    packages=['gdatacopier']
 )
